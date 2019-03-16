e307f88f4b112eefc4028446ed0554a70981c814
“shenxl112” <shenxl1102@gmail.com>
2019-03-15 23:39:01 -0400

    Added Counter

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,13 +1,15 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
-import android.os.Parcel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -25,15 +27,17 @@ public class ComposeActivity extends AppCompatActivity {
     private EditText etCompose;
     private Button btnTweet;
     private TwitterClient client;
+    private TextView tvCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
 
-        client = TwitterApp.getRestClient(this);
+        client    = TwitterApp.getRestClient(this);
         etCompose = findViewById(R.id.etCompose);
         btnTweet  = findViewById(R.id.btnTweet);
+        tvCount   = findViewById(R.id.tvCount);
 
         //Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
@@ -45,7 +49,7 @@ public class ComposeActivity extends AppCompatActivity {
                     Toast.makeText(ComposeActivity.this, "Your tweet is empty!", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if(tweetContent.length() > 140){
+                if(tweetContent.length() > MAX_TWEET_LENGTH){
                     Toast.makeText(ComposeActivity.this, "Your tweet is too long", Toast.LENGTH_LONG).show();
                     return;
                 }
@@ -77,5 +81,24 @@ public class ComposeActivity extends AppCompatActivity {
             }
         });
 
+        //EditText etValue = (EditText) findViewById(R.id.etValue);
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+                tvCount.setText(Integer.toString(140 - etCompose.length()));
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+            }
+        });
     }
 }
